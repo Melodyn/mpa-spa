197a00853d95131a6f24dbb70a8ad72b848bdad8
Melodyn <samelodyn@gmail.com>
2021-09-25 09:06:20 +0300

    added comments

--- spa-csr/index.js
+++ spa-csr/index.js
@@ -1,7 +1,13 @@
-const getPage = (name) => fetch(`http://127.0.0.1:3000/api/${name}`).then(res => res.json());
+/*
+  Из всего ниже идущего кода нас интересует только функция render.
+  Именно она обновляет HTML текущей страницы на основе данных,
+  поступивших с сервера в "чистом" виде.
+*/
+
+const getPage = (name) => fetch(`http://127.0.0.1:6700/api/${name}`).then(res => res.json());
 
 const initPagePath = 'main';
-const state = {
+const state = { // такой маленький, а уже React
   currentPage: '',
   previousPage: '',
   header: '',
@@ -12,44 +18,50 @@ document.addEventListener('DOMContentLoaded', () => {
   const ulEl = document.querySelector('ul');
   ulEl.style.display = 'none';
 
+  // главные действующие лица
   const headerEl = document.querySelector('h1');
   const buttons = document.querySelectorAll('li');
 
   // клиентский рендеринг (CSR)
   const render = () => {
     const { currentPage, previousPage, header } = state;
+    // рендеринг заголовка
     headerEl.textContent = header;
 
     const previousPageButtonEl = document.querySelector(`p`);
+    // рендеринг кнопки страницы, с которой ушли
     previousPageButtonEl.outerHTML = `<a href="/${previousPage}">${previousPageButtonEl.textContent}</a>`;
 
     const activePageButtonEl = document.querySelector(`a[href="/${currentPage}"]`);
+    // рендеринг кнопки (отключенной) на которую пришли
     activePageButtonEl.outerHTML = `<p>${activePageButtonEl.textContent}</p>`;
   };
 
+  // MVC на минималках
   buttons.forEach((button) => {
     button.addEventListener('click', async (e) => {
       e.preventDefault();
       const { tagName, href } = e.target;
-      if (tagName !== 'A') return;
+      if (tagName !== 'A') return; // некоторое допущение, что все кликают по ссылке
 
       const newPageName = new URL(href).pathname.replace('/', '');
-      const { header } = await getPage(newPageName);
+      // данные с сервера в формате JSON, преобразованные в JS-объект
+      const data = await getPage(newPageName);
 
       state.previousPage = state.currentPage;
       state.currentPage = newPageName;
-      state.header = header;
+      state.header = data.header;
 
       render();
     });
   });
 
-  // имитация загрузки страницы
+  // имитация загрузки страницы 2 секунды
   setTimeout(() => {
-    getPage(initPagePath).then(({ header }) => {
+    getPage(initPagePath).then((data) => {
       state.previousPage = initPagePath;
       state.currentPage = initPagePath;
-      state.header = header;
+      state.header = data.header;
 
       render();
 
--- spa-csr/server.js
+++ spa-csr/server.js
@@ -1,25 +1,29 @@
 const http = require('http');
 
-const params = {
-  port: 3000,
-  host: '127.0.0.1',
-};
-
+/*
+  Сервер отвечает данными в формате JSON.
+  Для простоты тут только название страницы.
+*/
 const pages = {
   main: { header: 'Главная' },
   about: { header: 'Обо мне' },
   contacts: { header: 'Контакты' },
 };
 
+// * можно не читать код отсюда
+const params = {
+  port: 6700,
+  host: '127.0.0.1',
+};
+
 http
   .createServer((req, res) => {
+    // CORS
     const headers = {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
       'Access-Control-Max-Age': 2592000,
     };
-
-    // CORS
     if (req.method === 'OPTIONS') {
       res.writeHead(204, headers);
       res.end();
@@ -31,16 +35,21 @@ http
       'Content-Type': 'application/json; charset=utf-8',
     });
 
+    // роутинг
     const route = req.url.match(/\/api\/(?<page>\w+)/);
-    if (!route) {
+    if (!route) { // Если запрос не к api, то сервер отвечает пустым объектом
       res.end(JSON.stringify({}));
       return;
     }
+// * досюда
 
+    // Сопоставляем запрос с данными
     const { groups: { page } } = route;
     const pageContent = pages[page] || {};
 
-    res.end(JSON.stringify(pageContent));
+    res.end(JSON.stringify(pageContent)); // и отдаём данные
+    // как их будет использовать клиент - сервер уже не знает
+    // рендеринг ложится на вычислительные мощности клиента
   })
   .listen(params.port, params.host, () => {
     console.info(`Server running on http://${params.host}:${params.port}`);
